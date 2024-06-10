a19aa565ac58110d2f9f6f7616578fbe9d6a84ec
wave46 <wave880@gmail.com>
2024-06-10 17:53:43 +0200

    uploading minor changes to remote from meso

--- hdg_postprocess/HDG_mesh.py
+++ hdg_postprocess/HDG_mesh.py
@@ -47,6 +47,8 @@ class HDGmesh:
         elif self.mesh_parameters['element_type'] == 'quadrilateral':
             if self.mesh_parameters['nodes_per_element']==49:
                 self._p_order = 6
+            elif self.mesh_parameters['nodes_per_element']==81:
+                self._p_order = 8
 
         minr,maxr,minz,maxz = 1e5,-1e5,1e5,-1e5
         for vertices in self.raw_vertices:
