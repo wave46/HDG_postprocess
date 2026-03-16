535b0b3404b22ac49aa03e004c3a65f68f3962a3
wave46 <59137482+wave46@users.noreply.github.com>
2026-03-16 18:06:23 +0100

    Simplify HDGmesh setup

--- hdg_postprocess/HDG_mesh.py
+++ hdg_postprocess/HDG_mesh.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from hdg_postprocess.mesh_api import (
     MeshBoundary,
     MeshBoundaryState,
@@ -14,12 +15,53 @@ class HDGmesh:
     The mesh is triangular (so far), high order  (so far p=4 or p=6), so it can have more than 3 nodes per element
     """
 
-    def __init__(self, raw_vertices,raw_connectivity,raw_connectivity_boundary,
-                raw_mesh_numbers,raw_boundary_flags,raw_ghost_elements,
-                raw_ghost_faces,mesh_parameters,n_partitions,raw_rest_mesh_data=None):
+    def __init__(
+        self,
+        raw_vertices,
+        raw_connectivity,
+        raw_connectivity_boundary,
+        raw_mesh_numbers,
+        raw_boundary_flags,
+        raw_ghost_elements,
+        raw_ghost_faces,
+        mesh_parameters,
+        n_partitions,
+        raw_rest_mesh_data=None,
+    ):
         """
         Here we just store the raw data
         """
+        self._store_raw_inputs(
+            raw_vertices,
+            raw_connectivity,
+            raw_connectivity_boundary,
+            raw_mesh_numbers,
+            raw_boundary_flags,
+            raw_ghost_elements,
+            raw_ghost_faces,
+            mesh_parameters,
+            n_partitions,
+        )
+        if n_partitions > 1:
+            if raw_rest_mesh_data is None:
+                raise ValueError("communication info is not provided")
+
+            self._raw_rest_mesh_data = raw_rest_mesh_data
+
+        self._initial_setup()
+
+    def _store_raw_inputs(
+        self,
+        raw_vertices,
+        raw_connectivity,
+        raw_connectivity_boundary,
+        raw_mesh_numbers,
+        raw_boundary_flags,
+        raw_ghost_elements,
+        raw_ghost_faces,
+        mesh_parameters,
+        n_partitions,
+    ):
         self._raw_vertices = raw_vertices
         self._raw_connectivity = raw_connectivity
         self._raw_connectivity_boundary = raw_connectivity_boundary
@@ -29,71 +71,66 @@ class HDGmesh:
         self._raw_ghost_faces = raw_ghost_faces
         self._mesh_parameters = mesh_parameters
         self._n_partitions = n_partitions
-        if n_partitions>1:
-            if raw_rest_mesh_data is None:
-                raise ValueError("communication info is not provided")
 
-            self._raw_rest_mesh_data = raw_rest_mesh_data
-
-        self._initial_setup()
-
-
-
-    def _initial_setup(self):
+    def _init_api_helpers(self):
         self._boundary = MeshBoundary(self)
         self._geometry = MeshGeometry(self)
         self._plot = MeshPlot(self)
+
+    def _init_state_containers(self):
         self._metadata = MeshMetadata()
         self._global_state = MeshGlobalState()
         self._derived_geometry = MeshDerivedGeometryState()
         self._boundary_state = MeshBoundaryState()
 
-        if self.mesh_parameters['element_type'] == 'triangle':
-            if self.mesh_parameters['nodes_per_element']==15:
-                self._metadata.p_order = 4
-            elif self.mesh_parameters['nodes_per_element']==28:
-                self._metadata.p_order = 6
-            elif self.mesh_parameters['nodes_per_element']==45:
-                self._metadata.p_order = 8
-        elif self.mesh_parameters['element_type'] == 'quadrilateral':
-            if self.mesh_parameters['nodes_per_element']==49:
-                self._metadata.p_order = 6
-            elif self.mesh_parameters['nodes_per_element']==81:
-                self._metadata.p_order = 8
+    def _infer_p_order(self):
+        if self.mesh_parameters["element_type"] == "triangle":
+            if self.mesh_parameters["nodes_per_element"] == 15:
+                return 4
+            if self.mesh_parameters["nodes_per_element"] == 28:
+                return 6
+            if self.mesh_parameters["nodes_per_element"] == 45:
+                return 8
+        elif self.mesh_parameters["element_type"] == "quadrilateral":
+            if self.mesh_parameters["nodes_per_element"] == 49:
+                return 6
+            if self.mesh_parameters["nodes_per_element"] == 81:
+                return 8
+        return None
 
-        minr,maxr,minz,maxz = 1e5,-1e5,1e5,-1e5
+    def _init_metadata(self):
+        self._metadata.p_order = self._infer_p_order()
+        minr, maxr, minz, maxz = 1e5, -1e5, 1e5, -1e5
         for vertices in self.raw_vertices:
-                       
-            minr = min(minr,vertices[:,0].min())
-            minz = min(minz,vertices[:,1].min())
-            maxr = max(maxr,vertices[:,0].max())
-            maxz = max(maxz,vertices[:,1].max())
-        self._metadata.extent = {"minr": minr, "maxr":maxr, 
-                                 "minz": minz, "maxz":maxz}
-        if self._n_partitions == 1:
-            #no need to combine meshes
+            minr = min(minr, vertices[:, 0].min())
+            minz = min(minz, vertices[:, 1].min())
+            maxr = max(maxr, vertices[:, 0].max())
+            maxz = max(maxz, vertices[:, 1].max())
+        self._metadata.extent = {"minr": minr, "maxr": maxr, "minz": minz, "maxz": maxz}
+
+    def _init_partition_state(self):
+        if self.n_partitions == 1:
             self._metadata.flags.combined_to_full = True
             self._global_state.connectivity = self.raw_connectivity[0]
             self._global_state.vertices = self.raw_vertices[0]
             self._global_state.n_elements = self._global_state.connectivity.shape[0]
             self._global_state.n_vertices = self._global_state.vertices.shape[0]
             self._global_state.n_faces = None
-            self._metadata.flags.boundary_combined = False
-            
-
-        
-        else:    
+        else:
             self._metadata.flags.combined_to_full = False
             self._global_state.connectivity = None
             self._global_state.vertices = None
             self._global_state.n_elements = None
             self._global_state.n_vertices = None
             self._global_state.n_faces = None
-            self._metadata.flags.boundary_combined = False
+        self._metadata.flags.boundary_combined = False
 
-            
-            
-     
+    def _initial_setup(self):
+        self._init_api_helpers()
+        self._init_state_containers()
+        self._init_metadata()
+        self._init_partition_state()
+
     @property
     def raw_vertices(self):
         """raw vertices"""
@@ -110,50 +147,9 @@ class HDGmesh:
         return self._global_state.vertices
 
     @property
-    def vertices_gauss(self):
-        """coordinates of gauss points in global mesh"""
-        return self._derived_geometry.vertices_gauss
-
-    @property
     def connectivity_glob(self):
         """connectivity of a global mesh"""
         return self._global_state.connectivity
-
-    @property
-    def connectivity_b_glob(self):
-        """connectivity of faces on global mesh. filled only with boundary faces"""
-        return self._boundary_state.connectivity
-    
-    @property
-    def boundary_flags(self):
-        """
-            faces flags on a full mesh.
-            -1 means that it is inside boundary, not filled int
-            0 boundary between two partitions
-            to be done: fill other flags
-        """
-        return self._boundary_state.flags
-
-    @property
-    def face_element_number(self):
-        """
-        for each face gives a number of the corresponding element
-        """
-        return self._boundary_state.face_element_number
-
-    @property
-    def face_local_number(self):
-        """
-        for each face gives a local number of the face in corresponding element
-        """
-        return self._boundary_state.face_local_number
-
-    @property
-    def face_ghost(self):
-        """
-        If face corresponds to ghost element
-        """
-        return None
 
     @property
     def nelems_glob(self):
